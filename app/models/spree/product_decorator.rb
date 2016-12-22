caa4cf5015527a2217df605701180f313e739e0e
Alex Costantini <alexcostantini@alexcostantini.local>
2016-12-21 22:53:34 -0500

    wip

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,15 +1,16 @@
 Spree::Product.class_eval do
+  after_create :set_supplier
 
   has_many :suppliers, through: :master
 
   def add_supplier!(supplier_or_id)
-    supplier = supplier_or_id.is_a?(Spree::Supplier) ? supplier_or_id : Spree::Supplier.find(supplier_or_id)
-    populate_for_supplier! supplier if supplier
+    splr = supplier_or_id.is_a?(Spree::Supplier) ? supplier_or_id : Spree::Supplier.find(supplier_or_id)
+    populate_for_supplier! splr if splr
   end
 
   def add_suppliers!(supplier_ids)
-    Spree::Supplier.where(id: supplier_ids).each do |supplier|
-      populate_for_supplier! supplier
+    Spree::Supplier.where(id: supplier_ids).each do |splr|
+      populate_for_supplier! splr
     end
   end
 
@@ -27,6 +28,10 @@ Spree::Product.class_eval do
         supplier.stock_locations.each { |location| location.propagate_variant(variant) }
       end
     end
+  end
+
+  def set_supplier
+    add_supplier! self.supplier
   end
 
 end
